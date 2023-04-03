6c9922b7b6e776ff2ff7968602b4928d03870c9b
eeching <xuyiqing613@gamil.com>
2023-04-03 10:43:43 +0000

    test generalization

--- visualize_table.py
+++ visualize_table.py
@@ -8,13 +8,16 @@ import json
 import random
 
 
-def get_single_tidy_config(episode="single"):
+def get_single_tidy_config(episode="single", with_napkin=False):
 
     base_path = f"./exps/{episode}"
     os.makedirs(base_path, exist_ok = True)
     obs_counter = 0
 
-    object_list, grid = object_spec.get_tidy_config(num_cup1=0, num_cup2=6, num_uten=8, horizontal=True, stick_to_left=True, num_napkin=0, num_plate=4)
+    if with_napkin:
+        num_napkin = 5
+
+    object_list, grid = object_spec.get_tidy_config(num_cup1=0, num_cup2=6, num_uten=8, horizontal=True, stick_to_left=True, num_napkin=num_napkin)
     np.savetxt(f'{base_path}/grid.out', grid)
                         
     pdb.set_trace()
@@ -56,6 +59,39 @@ def get_single_tidy_config(episode="single"):
             sim.get_observation_nvisii_cliport(f"{base_path}/{obs_counter}/clip/")
             obs_counter += 1
 
+def single_random_walk(episode="single", with_napkin=True):
+    
+    num_cup1, num_cup2, num_uten, num_napkin, horizontal, stick_to_left  = 9, 6, 8, 5, True, True
+    object_list, grid = object_spec.get_tidy_config(num_cup1=num_cup1, num_cup2=num_cup2, num_uten=num_uten, horizontal=horizontal, stick_to_left=stick_to_left, num_napkin=num_napkin)
+    base_path = f"./exps/{episode}/with_napkin_{with_napkin}/cup1_{num_cup1}_cup2_{num_cup2}_fork_{num_uten}_napkin_{num_napkin}_hori_{horizontal}_left_{stick_to_left}"
+    os.makedirs(base_path, exist_ok = True)
+
+    sim = Tabletop_Sim(
+            width=640,
+            height=640,
+            indivisual_loading=True,
+    )
+
+    sim = init_config(sim, object_list)
+    sim.get_observation_nvisii(f"{base_path}/0")
+    sim.get_observation_nvisii_cliport(f"{base_path}/0/clip/")
+
+    random.shuffle(object_list)
+
+    for t in range(len(object_list)):
+        obj = object_list[t] 
+        obj, grid = object_spec.random_walk_one_step(obj, grid)
+        object_list[t] = obj
+        sim.reset_obj_pose(name=obj["name"], 
+            size=obj_dict.default_size[obj["type"]], 
+            position=obj["pos"], 
+            baseOrientationAngle=0)
+        
+        sim.get_observation_nvisii(f"{base_path}/{t+1}")
+        sim.get_observation_nvisii_cliport(f"{base_path}/{t+1}/clip/")
+
+
+
 def batch_tidy_config(episode="batch_init", render=True, with_napkin=False):
   
     base_path = f"./exps/{episode}/with_napkin_{with_napkin}"
@@ -90,15 +126,17 @@ def batch_tidy_config(episode="batch_init", render=True, with_napkin=False):
         sim.get_observation_nvisii_cliport(f"{curr_path}/clip/")
 
 def batch_random_walk(episode="batch_traj", with_napkin=False):
-    root_path = f"./exps/{episode}/with_napkin_{with_napkin}"
-    os.makedirs(root_path, exist_ok = True)
+    base_path = f"./exps/{episode}/with_napkin_{with_napkin}"
+    os.makedirs(base_path, exist_ok = True)
 
     with open(f'./exps/batch_init//with_napkin_{with_napkin}/config.json', 'r') as f:
         batch_tidy_config = json.load(f)
                         
-    pdb.set_trace()
     num_configs = len(batch_tidy_config)
     print(f"{num_configs} initial tidy configurations" )
+    batch_tidy_config_sub_idx = np.random.choice(len(batch_tidy_config), 5, replace=False)
+    pdb.set_trace()
+    
 
     sim = Tabletop_Sim(
             width=640,
@@ -106,21 +144,21 @@ def batch_random_walk(episode="batch_traj", with_napkin=False):
             indivisual_loading=True,
     )
 
-    for conf in batch_tidy_config:
+    for idx in batch_tidy_config_sub_idx:
+        conf = batch_tidy_config[idx]
         object_list, grid, spec = conf
         grid = np.array(grid)
         if with_napkin:
-            base_path = f"{base_path}/cup1_{spec[0]}_cup2_{spec[1]}_fork_{spec[2]}_napkin_{spec[3]}_hori_{spec[4]}_left_{spec[5]}"
+            curr_path = f"{base_path}/cup1_{spec[0]}_cup2_{spec[1]}_fork_{spec[2]}_napkin_{spec[3]}_hori_{spec[4]}_left_{spec[5]}"
         else:
-            base_path = f"{root_path}/cup1_{spec[0]}_cup2_{spec[1]}_fork_{spec[2]}_hori_{spec[3]}_left_{spec[4]}"
+            curr_path = f"{base_path}/cup1_{spec[0]}_cup2_{spec[1]}_fork_{spec[2]}_hori_{spec[3]}_left_{spec[4]}"
         os.makedirs(base_path, exist_ok = True)
 
         # initialize the tidy configuration
         sim = init_config(sim, object_list)
-        sim.get_observation_nvisii(f"{base_path}/0.png")
 
-        sim.get_observation_nvisii(f"{base_path}/0")
-        sim.get_observation_nvisii_cliport(f"{base_path}/0/clip/")
+        sim.get_observation_nvisii(f"{curr_path}/0")
+        sim.get_observation_nvisii_cliport(f"{curr_path}/0/clip/")
 
         random.shuffle(object_list)
 
@@ -133,8 +171,8 @@ def batch_random_walk(episode="batch_traj", with_napkin=False):
                 position=obj["pos"], 
                 baseOrientationAngle=0)
         
-            sim.get_observation_nvisii(f"{base_path}/{t+1}")
-            sim.get_observation_nvisii_cliport(f"{base_path}/{t+1}/clip/")
+            sim.get_observation_nvisii(f"{curr_path}/{t+1}")
+            sim.get_observation_nvisii_cliport(f"{curr_path}/{t+1}/clip/")
         # pdb.set_trace()
 
                
@@ -165,6 +203,8 @@ def init_config(sim, object_list):
             print(f"Collison!")
     return sim
 
-get_single_tidy_config()
-# batch_tidy_config(render=False)
-# batch_random_walk()
+# get_single_tidy_config()
+# single_random_walk()
+# batch_tidy_config(render=False, with_napkin=True)
+batch_random_walk(with_napkin=True)
+
